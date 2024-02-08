8894ca2c96dc5fb71626effbb1fb5ebea50dc70b
ValentinaGiraldo03 <Valegiraldo.3@hotmail.com>
2024-02-08 11:26:38 -0500

    cambios en la funcion about

--- movie/views.py
+++ movie/views.py
@@ -10,5 +10,6 @@ def home(request):
 
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    #return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
 
