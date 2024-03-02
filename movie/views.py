d3fc4ffab6098f4bb3bfc4ed477dc7b4b3a5694b
ValentinaGiraldo03 <Valegiraldo.3@hotmail.com>
2024-03-01 20:51:36 -0500

    Segundo Taller (Frontend y BD

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Movie
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
 
 # Create your views here.
 
@@ -21,3 +24,106 @@ def home(request):
 def about(request):
     #return HttpResponse('<h1>Welcome to About Page</h1>')
     return render(request, 'about.html')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
+
+'''Este código ubicado aquí abajo es el del gráfico de películas por año'''
+# Versión más eficiente
+def statistics_view(request):
+    matplotlib.use('Agg')
+    # Obtener todas las películas
+    all_movies = Movie.objects.all()
+
+    # Crear un diccionario para alamcenar la cantidad de películas por año
+    movie_counts_by_year = {}
+
+    # Filtrar las películas por año y contar la cantidad de películas por año
+    for movie in all_movies:
+        year = movie.year if movie.year else "None"
+        if year in movie_counts_by_year:
+            movie_counts_by_year[year] += 1
+        else:
+            movie_counts_by_year[year] = 1
+    
+    # Ancho de las barras 
+    bar_width = 0.5
+    # Posiciones de las barras
+    bar_positions = range(len(movie_counts_by_year))
+    # Crear la gráfica de barras
+    plt.bar(bar_positions, movie_counts_by_year.values(), width = bar_width, align = 'center')
+
+    # Personalizar la gráfica 
+    plt.title('Movie per year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation = 90)
+
+    # Ajustar el epsacio entre las barras 
+    plt.subplots_adjust(bottom = 0.3)
+
+    # Guardar la gráfica en un objeto BytesIO
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format = 'png')
+    buffer.seek(0)
+    plt.close()
+
+    # Convertir la gráfica a base64
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png)
+    graphic = graphic.decode ('utf-8')
+
+    # Renderizar la plantilla statistics.html con la gráfica
+    return render (request, 'statistics.html', {'graphic' : graphic})
+
+
+'''Este código ubicado aquí abajo es el del gráfico de películas por género'''
+# Versión más eficiente
+def statistics_genre_view(request):
+    matplotlib.use('Agg')
+    # Obtener todas las películas
+    all_movies = Movie.objects.all()
+
+    # Crear un diccionario para alamcenar la cantidad de películas por género
+    movie_counts_by_genre = {}
+
+    # Filtrar las películas por género y contar la cantidad de películas por género
+    for movie in all_movies:
+        genre = movie.genre.split(',')[0].strip() if movie.genre else "None"
+        if genre in movie_counts_by_genre:
+            movie_counts_by_genre[genre] += 1
+        else:
+            movie_counts_by_genre[genre] = 1
+    
+    # Ancho de las barras 
+    bar_width = 0.5
+    # Posiciones de las barras
+    bar_positions = range(len(movie_counts_by_genre))
+    # Crear la gráfica de barras
+    plt.bar(bar_positions, movie_counts_by_genre.values(), width = bar_width, align = 'center')
+
+    # Personalizar la gráfica 
+    plt.title('Movie per genre')
+    plt.xlabel('Genre')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_genre.keys(), rotation = 90)
+
+    # Ajustar el epsacio entre las barras 
+    plt.subplots_adjust(bottom = 0.3)
+
+    # Guardar la gráfica en un objeto BytesIO
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format = 'png')
+    buffer.seek(0)
+    plt.close()
+
+    # Convertir la gráfica a base64
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png)
+    graphic = graphic.decode ('utf-8')
+
+    # Renderizar la plantilla statistics.html con la gráfica
+    return render (request, 'statistics.html', {'graphic' : graphic})
